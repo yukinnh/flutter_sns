abff0295174cd987a41ee60cc4d97e9e49688ba0
yukin <yukinnh19950123@gmail.com>
2022-08-29 22:17:46 +0900

    コードを整理

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:sns_application/view/time_line/time_line_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  ss
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TimeLinePage(),
+    );
+  }
+}
